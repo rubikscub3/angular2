3bc24be23c2196f89d52d66ba780f42b928f1f96
unknown <aatir.farooqui@gmail.com>
2018-01-19 17:45:56 +0500

    Signed-off-by: unknown <aatir.farooqui@gmail.com>

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id:11. name :'Aatir'},
+    {id:12. name :'Tahamie'},
+    {id:13. name :'Ameem'},
+    {id:14. name :'Basil'}
+];
